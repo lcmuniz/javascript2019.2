11c91200b8a091f0c852aab240d6ce75c91e6834
Luiz Carlos Melo Muniz <lcmuniz@gmail.com>
2019-11-05 22:51:08 -0300

    matrizes

---
+++ arrays01/somar_matrizes.js
@@ -0,0 +1,39 @@
+// funcao somarMatrizes:
+// recebe como entrada duas matrizes A e B
+// e retorna uma matriz que eh a soma A + B
+function somarMatrizes(A, B) {
+
+  // verifica se A e B são matrizes e retorna NaN se não forem
+  const aEhMatriz = Array.isArray(A) && Array.isArray(A[0])
+  const bEhMatriz = Array.isArray(B) && Array.isArray(B[0])
+  if (!aEhMatriz || !bEhMatriz) {
+    return NaN
+  }
+
+  // verifica se o numero de linhas e colunas das matrizes sao iguais
+  // retorna NaN se nao forem
+  const linA = A.length
+  const colA = A[0].length
+  const linB = B.length
+  const colB = B[0].length
+  if (linA != linB || colA != colB) {
+    return NaN
+  }
+  
+  // soma as duas matrizes
+  const C = []
+  for (let l = 0; l < linA; l++) {
+    linha = []
+    for (let c = 0; c < colA; c++) {
+      const elem = A[l][c] + B[l][c]
+      linha.push(elem)
+    }
+    C.push(linha)
+  }
+  //retorna a matriz C, soma de A e B
+  return C
+  
+}
+
+x = somarMatrizes([[1,2],[2,3],[1,1]], [[2,3],[1,1],[3,3]])
+console.log(x)
